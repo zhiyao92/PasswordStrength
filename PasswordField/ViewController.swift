7658a2b98300397b7f0f2bdeb0765a672cef969c
Kelvin Tan <kelvin.vins@gmail.com>
2018-10-20 11:09:15 +0800

    first commit

--- PasswordField/ViewController.swift
+++ PasswordField/ViewController.swift
@@ -10,11 +10,48 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var passwordStrength: UILabel!
+    @IBOutlet weak var showHideLabel: UIButton!
+    
+    var passwordVisible: Bool = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+       configureView()
+    }
+    
+    func configureView(){
+        passwordTextField.isSecureTextEntry = true
+        passwordTextField.clearsOnBeginEditing = false
+        showHideLabel.setTitle("Show", for: .normal)
+        passwordStrength.isHidden = true
     }
 
-
+    @IBAction func showHide() {
+        if passwordVisible {
+            passwordTextField.isSecureTextEntry = false
+            showHideLabel.setTitle("Hide", for: .normal)
+            passwordVisible = false
+        } else {
+            passwordTextField.isSecureTextEntry = true
+            showHideLabel.setTitle("Show", for: .normal)
+            passwordVisible = true
+        }
+    }
+    
+    @IBAction func passwordTyped() {
+        passwordStrength.isHidden = false
+        if (passwordTextField.text?.count)! < 3 {
+            passwordStrength.text = "Password is weak"
+            passwordStrength.textColor = UIColor.red
+        } else if (passwordTextField.text?.count)! > 8 {
+            passwordStrength.text = "Password is unbreakable"
+            passwordStrength.textColor = UIColor.green
+        } else {
+            passwordStrength.text = "Password is safer"
+            passwordStrength.textColor = UIColor.purple
+        }
+    }
 }
 
